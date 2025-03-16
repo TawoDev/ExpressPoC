61cb3ce5cb3daffb3b3e3be999421a410ec11073
TawoDev <tawo.dev@outlook.com>
2025-03-16 17:11:05 +0000

    update the sessionkey

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.post('/chat/init', async (req, res) => {
     const token = await accessToken(clientId, clientSecret);
 
     const requestData = {
-        // externalSessionKey: "123456",
+        externalSessionKey: "123456",
         instanceConfig: {
             endpoint: orgUrl
         },
