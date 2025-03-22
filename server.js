8dfdbe42f5f9b7af328b1fb08a01cbbc9685ef13
TawoDev <tawo.dev@outlook.com>
2025-03-22 11:30:05 +0000

    add console log

--- server.js
+++ server.js
@@ -54,6 +54,7 @@ app.get('/healthz', (req, res) => {
 });
 
 app.post('/chat/init', async (req, res) => {
+    console.log("Session before:", JSON.stringify(req.session));
     const token = await accessToken(clientId, clientSecret);
 
     const requestData = {
@@ -95,6 +96,7 @@ app.post('/chat/init', async (req, res) => {
 });
 
 app.post('/chat/cont', async (req, res) => {
+    console.log(`req.session--${req.session}`);
     if (!req.session.access_token || !req.session.sessionId) {
         console.log(`req.session.access_token--->${req.session.access_token}`);
         console.log(`req.session.sessionId--->${req.session.sessionId}`);
