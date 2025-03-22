cac24664ab5b243fda8e4a80b93d303ac8142af8
TawoDev <tawo.dev@outlook.com>
2025-03-22 10:34:53 +0000

    Add requrie express-session

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const dotenv = require('dotenv');
+const session = require('express-session');
 dotenv.config();
 
 const apiUrl = process.env.API_URL;
